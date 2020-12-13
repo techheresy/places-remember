6422a77cb79ae4e9b497b958c4b821e5e4b0c21c
techheresy <techheresy@gmail.com>
2020-12-13 16:42:26 +0500

    Init project structure

---
+++ README.md
@@ -0,0 +1,6 @@
+## Places Remember
+
+[![code formatter: black](https://img.shields.io/badge/formatter-black-black)](https://github.com/psf/black)
+[![code style: we-make-service](https://img.shields.io/badge/style-wemake-000000.svg)](https://github.com/wemake-services/wemake-python-styleguide)
+
+Homework for the company [Very Interesting](https://www.interesnee.ru/)
---
+++ backend/config/settings.py
@@ -0,0 +1,165 @@
+import environ
+
+ROOT_DIR = environ.Path(__file__) - 2
+
+env = environ.Env()
+env.read_env()
+
+DJANGO_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.admin",
+    "django.contrib.gis",
+    "django.contrib.sites",
+]
+
+THIRD_PARTY_APPS = [
+    'corsheaders',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'rest_auth.registration',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+    'django_extensions',
+]
+
+LOCAL_APPS = [
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+SITE_ID = 1
+
+DEBUG = env.bool("DEBUG")
+
+SECRET_KEY = env.str("SECRET_KEY")
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
+
+DOMAIN = env.str("DOMAIN")
+
+ADMINS = [("admin", env.str("ADMIN_EMAIL"))]
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": env.str("POSTGRES_DB"),
+        "USER": env.str("POSTGRES_USER"),
+        "PASSWORD": env.str("POSTGRES_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": 5432,
+    }
+}
+
+TIME_ZONE = "UTC"
+
+LANGUAGE_CODE = "en-us"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_ROOT = str(ROOT_DIR("staticfiles"))
+
+STATIC_URL = "/staticfiles/"
+
+STATICFILES_DIRS = [str(ROOT_DIR("static"))]
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+MEDIA_ROOT = str(ROOT_DIR("media"))
+
+MEDIA_URL = "/media/"
+
+ROOT_URLCONF = "config.urls"
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": STATICFILES_DIRS,
+        "OPTIONS": {
+            "debug": DEBUG,
+            "loaders": [
+                "django.template.loaders.filesystem.Loader",
+                "django.template.loaders.app_directories.Loader",
+            ],
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    }
+]
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
+
+REST_USE_JWT = True
+
+REST_FRAMEWORK = {
+    "UPLOADED_FILES_USE_URL": False,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
+        "rest_framework.parsers.FileUploadParser",
+    ],
+}
+
+
+# TODO: Create Serializer
+# REST_AUTH_SERIALIZERS = {"USER_DETAILS_SERIALIZER": ""}
+
+# TODO: Create Model
+# AUTH_USER_MODEL = ""
