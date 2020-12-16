31626222e2cd13e424e4ab39f47505930196784d
techheresy <techheresy@gmail.com>
2020-12-17 02:56:36 +0500

    Fixed axios JWT header

--- frontend/src/utils/setAuthToken.js
+++ frontend/src/utils/setAuthToken.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function setAuthToken(token) {
   if (token) {
-    axios.defaults.headers.common["Authorization"] = token;
+    axios.defaults.headers.common["Authorization"] = `JWT ${token}`;
   } else {
     delete axios.defaults.headers.common["Authorization"];
   }
