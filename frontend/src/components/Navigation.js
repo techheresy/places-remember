f77e7197b1bf1f07261daf3e97c81701cb10ac04
techheresy <techheresy@gmail.com>
2020-12-18 00:44:42 +0500

    Added navlink highlighting logic

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,35 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar, NavDropdown, Nav, NavLink } from "react-bootstrap";
 import { connect } from "react-redux";
+import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
 
 import { userLogout } from "../redux/auth/authActions";
 
 function Navigation(props) {
   const { auth, userLogout } = props;
+  const { firstName, lastName, photoUrl } = auth.info;
+
+  useEffect(() => {}, [props.location]);
 
   if (auth.isAuthenticated) {
-    const { firstName, lastName, photoUrl } = auth.info;
-
     return (
-      <Navbar bg="primary" variant="dark" expand="lg">
+      <Navbar bg="primary" variant="dark" expand="lg" fixed="top">
         <Navbar.Brand>
           <img
             alt=""
             src={photoUrl}
             width="30"
             height="30"
-            className="d-inline-block align-top"
+            className="d-inline-block align-top rounded"
           />{" "}
           {firstName} {lastName}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto" defaultActiveKey="list">
-            <NavLink as={Link} eventKey="list" to="/list">
+          <Nav className="mr-auto" activeKey={window.location.pathname}>
+            <NavLink as={Link} eventKey="/list" to="/list">
               My places
             </NavLink>
-            <NavLink as={Link} eventKey="add" to="/add">
+            <NavLink as={Link} eventKey="/add" to="/add">
               Add place
             </NavLink>
           </Nav>
@@ -49,4 +51,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { userLogout })(Navigation);
+export default withRouter(connect(mapStateToProps, { userLogout })(Navigation));
