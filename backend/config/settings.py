36ea4dc743d6039d89e5b3253ee8cc5f01cd38dd
techheresy <techheresy@gmail.com>
2020-12-24 00:53:30 +0500

    Try fix label apps bug

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,7 +32,7 @@ THIRD_PARTY_APPS = [
     "django_extensions",
 ]
 
-LOCAL_APPS = ["apps.places", "apps.users", "apps.tools"]
+LOCAL_APPS = ["places", "users", "tools"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("apps.authentication.urls")),
-    path("api/", include("apps.places.urls")),
+    path("auth/", include("authentication.urls")),
+    path("api/", include("places.urls")),
 ]
